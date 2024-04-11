02b0efc7d25b98bf4ab31fc6847dbd99ab89fcf3
InfoyTech <90406016+PSGitHubUser1@users.noreply.github.com>
2024-04-11 15:04:12 +0530

    Add files via upload

---
+++ deai.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+clear
+echo ==================================================================
+sudo apt update
+sudo apt upgrade -y
+echo ==================================================================
+echo 
+echo Installing Nala [if supported by your distro]
+echo 
+sudo apt install nala
+echo ==================================================================
+echo 
+echo Installing Neofetch
+echo 
+sudo apt install neofetch
+echo ==================================================================
+echo 
+echo Installing Htop
+echo 
+sudo apt install htop
+echo ==================================================================
+echo 
+echo Installing Grub Customizer 
+echo 
+echo Adding repo. [Grub Customizer]
+echo 
+sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer
+echo ==================================================================
+echo 
+sudo apt install grub-customizer
+echo ==================================================================
+echo 
+echo Installing Flatpak
+echo 
+echo Adding repo. [Flatpak]
+sudo add-apt-repository -y ppa:flatpak/stable
+echo ==================================================================
+echo 
+#echo Updating packages...
+#sudo apt update
+#echo ===================================================================
+sudo apt install flatpak
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo 
+echo 
+echo Installing Software [Its like MS Store but for Debian/Linux]
+echo 
+sudo apt install gnome-software-plugin-flatpak
+echo ==================================================================
+echo 
+echo Installing Nano [In-Terminal editor]
+echo 
+sudo apt install nano
+echo ==================================================================
+echo 
+echo Installing Python 
+echo 
+sudo apt install python3
+echo ==================================================================
+echo 
+echo Installing Git
+echo 
+sudo apt install git
+echo ==================================================================
+echo Updating & Upgrading packages...
+echo 
+sudo apt update
+echo ==================================================================
+echo 
+sudo apt upgrae -y
+echo ==================================================================
+reboot
